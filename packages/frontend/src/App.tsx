ea68d34c44f2edf9b0a7771d9e37e26de13e562c
Clément <clement.pasek@gmail.com>
2023-02-28 18:06:32 +0100

    chore: update frontend to use apollo client with typescript

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import { useQuery, gql } from '@apollo/client';
-import reactLogo from './assets/react.svg'
+import { useQuery } from '@apollo/client';
+import { gql } from './__generated__/gql';
 import './App.css'
+import { GetUsersQuery } from './__generated__/graphql';
 
-const GET_USERS = gql`
+const GET_USERS = gql(/* GraphQL */ `
   query GetUsers {
     users {
       firstName
@@ -11,14 +12,15 @@ const GET_USERS = gql`
       email
     }
   }
-`;
+`);
 
 function App() {
-  const { loading, error, data } = useQuery(GET_USERS);
+  const { loading, error, data } = useQuery<GetUsersQuery>(GET_USERS);
   if (loading) return <h3>Loading...</h3>
   if (error) return <div>An error has occured !</div>
+  if (!data) return <div>No data</div>
 
-  return data.users.map((user: any) => <div>{user.firstName} {user.lastName} {user.email}</div>)
+  return data.users.map(user => <div>{user.firstName} {user.lastName} {user.email}</div>)
 }
 
 export default App
--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -10,7 +10,7 @@ const typeDefs = `#graphql
   }
 
   type Query {
-    users: [User]
+    users: [User!]!
   }
 `;
 
